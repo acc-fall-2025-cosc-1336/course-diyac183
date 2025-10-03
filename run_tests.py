32ce8a2f1bfa157e4d0bf65b36855714175f8ec2
diyac183 <diya.chandrasekhar@g.austincc.edu>
2025-10-03 01:51:58 +0000

    hw04 menu-driven-program

--- run_tests.py
+++ run_tests.py
@@ -3,6 +3,7 @@ import unittest
 the file in /tests/homework/b_in_proc_out/tests_in_proc_out
 has the test functions
 '''
-from tests.examples.a_example import tests_devprocess 
-suite = unittest.TestLoader().loadTestsFromModule(tests_devprocess)
+from tests.homework.d_repetition import tests_repetition 
+tests_repetition
+suite = unittest.TestLoader().loadTestsFromModule(tests_repetition)
 unittest.TextTestRunner(verbosity=2).run(suite)
