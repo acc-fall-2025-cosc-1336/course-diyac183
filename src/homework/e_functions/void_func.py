1de0d3bf7fe2e7a3b4b50a0732c5d2dcb95208d7
diyac183 <diya.chandrasekhar@g.austincc.edu>
2025-11-12 16:17:43 +0000

    hw05 code

--- src/homework/e_functions/void_func.py
+++ src/homework/e_functions/void_func.py
@@ -1,1 +1,6 @@
-#
+#In the void_functions.py write the display_payroll_check function ...displays: 
+#Gross Pay:100.00, FICA:7.65,Federal Tax:8.00, Net Pay:84.35
+def display_payroll_check(gross_pay, fica_tax, federal_tax, net_pay):
+    """Display"""
+    print(f"Gross Pay: {gross_pay:100}, FICA: {fica_tax:0.0765}, Federal Tax: {federal_tax:0.08}, Net Pay: {net_pay:0.8435}")
+
