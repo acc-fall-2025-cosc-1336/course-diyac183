c3cfe85cac1e73fd6842c8920ae32d0cf3a14c8c
diyac183 <diya.chandrasekhar@g.austincc.edu>
2025-11-18 15:13:31 +0000

    hw08 code

--- tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
+++ tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
@@ -1,21 +1,36 @@
+import unittest
 import sys
 sys.path.append("./")
-from src.homework.g_lists_and_tuples.lists import get_lowest_list_value 
+from src.homework.g_lists_and_tuples.lists import get_p_distance, get_p_distance_matrix
 
-import sys
-sys.path.append("./")
-from src.homework.g_lists_and_tuples.lists import lowest_value
 
-def test_get_lowest_list_value():
-    assert lowest_value(8, 10, 1, 50, 20) == 1
+class Test_Config(unittest.TestCase):
+    def test_p_distance(self):
+        a = ['T','T','T','C','C','A','T','T','T','A']
+        b = ['G','A','T','T','C','A','T','T','T','C']
+        self.assertAlmostEqual(get_p_distance(a, b), 0.4, places=3)
 
-    import sys
-sys.path.append("./")
-from src.homework.g_lists_and_tuples.lists import get_highest_list_value 
+    def test_get_p_distance_matrix(self):
+        data = [
+            ['T','T','T','C','C','A','T','T','T','A'],
+            ['G','A','T','T','C','A','T','T','T','C'],
+            ['T','T','T','C','C','A','T','T','T','T'],
+            ['G','T','T','C','C','A','T','T','T','A']
+        ]
 
-import sys
-sys.path.append("./")
-from src.homework.g_lists_and_tuples.lists import highest_value
+        expected = [
+            [0.0, 0.4, 0.1, 0.1],
+            [0.4, 0.0, 0.4, 0.3],
+            [0.1, 0.4, 0.0, 0.2],
+            [0.1, 0.3, 0.2, 0.0]
+        ]
 
-def test_get_highest_list_value():
-    assert highest_value(8, 10, 1, 50, 20) == 50
+        result = get_p_distance_matrix(data)
+        # compare element-wise with tolerance
+        for i in range(len(expected)):
+            for j in range(len(expected)):
+                self.assertAlmostEqual(result[i][j], expected[i][j], places=3)
+
+
+if __name__ == '__main__':
+    unittest.main()
